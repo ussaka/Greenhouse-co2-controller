b9b04cfa5d9010000dc9e6e4a613cab0b12e02cb
Keijo Länsikunnas <lansk@metropolia.fi>
2022-11-22 14:28:30 +0200

    Initial commit

---
+++ esp_socket/src/esp_test.cpp
@@ -0,0 +1,117 @@
+/*
+===============================================================================
+ Name        : main.c
+ Author      : $(author)
+ Version     :
+ Copyright   : $(copyright)
+ Description : main definition
+===============================================================================
+*/
+
+#if defined (__USE_LPCOPEN)
+#if defined(NO_BOARD_LIB)
+#include "chip.h"
+#else
+#include "board.h"
+#endif
+#endif
+
+#include <cr_section_macros.h>
+
+// TODO: insert other include files here
+#include "FreeRTOS.h"
+#include "task.h"
+#include "semphr.h"
+#include "heap_lock_monitor.h"
+#include "retarget_uart.h"
+
+#include "ModbusRegister.h"
+#include "DigitalIoPin.h"
+
+// TODO: insert other definitions and declarations here
+
+/* The following is required if runtime statistics are to be collected
+ * Copy the code to the source file where other you initialize hardware */
+extern "C" {
+
+void vConfigureTimerForRunTimeStats( void ) {
+	Chip_SCT_Init(LPC_SCTSMALL1);
+	LPC_SCTSMALL1->CONFIG = SCT_CONFIG_32BIT_COUNTER;
+	LPC_SCTSMALL1->CTRL_U = SCT_CTRL_PRE_L(255) | SCT_CTRL_CLRCTR_L; // set prescaler to 256 (255 + 1), and start timer
+}
+
+}
+/* end runtime statictics collection */
+
+static void idle_delay()
+{
+	vTaskDelay(1);
+}
+
+void task1(void *params)
+{
+	(void) params;
+
+	retarget_init();
+
+	ModbusMaster node3(241); // Create modbus object that connects to slave id 241 (HMP60)
+	node3.begin(9600); // all nodes must operate at the same speed!
+	node3.idle(idle_delay); // idle function is called while waiting for reply from slave
+	ModbusRegister RH(&node3, 256, true);
+
+	DigitalIoPin relay(0, 27, DigitalIoPin::output); // CO2 relay
+	relay.write(0);
+
+	DigitalIoPin sw_a2(1, 8, DigitalIoPin::pullup, true);
+	DigitalIoPin sw_a3(0, 5, DigitalIoPin::pullup, true);
+	DigitalIoPin sw_a4(0, 6, DigitalIoPin::pullup, true);
+	DigitalIoPin sw_a5(0, 7, DigitalIoPin::pullup, true);
+
+	while(true) {
+		float rh = 0;
+
+		vTaskDelay(2000);
+
+		rh = RH.read()/10.0;
+		printf("RH=%.1f%%\n", rh);
+	}
+}
+
+extern "C" {
+  void vStartSimpleMQTTDemo( void ); // ugly - should be in a header
+}
+
+int main(void) {
+
+#if defined (__USE_LPCOPEN)
+    // Read clock settings and update SystemCoreClock variable
+    SystemCoreClockUpdate();
+#if !defined(NO_BOARD_LIB)
+    // Set up and initialize all required blocks and
+    // functions related to the board hardware
+    Board_Init();
+    // Set the LED to the state of "On"
+    Board_LED_Set(0, true);
+#endif
+#endif
+
+	heap_monitor_setup();
+
+	// initialize RIT (= enable clocking etc.)
+	//Chip_RIT_Init(LPC_RITIMER);
+	// set the priority level of the interrupt
+	// The level must be equal or lower than the maximum priority specified in FreeRTOS config
+	// Note that in a Cortex-M3 a higher number indicates lower interrupt priority
+	//NVIC_SetPriority( RITIMER_IRQn, configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY + 1 );
+
+	xTaskCreate(task1, "test",
+			configMINIMAL_STACK_SIZE * 4, NULL, (tskIDLE_PRIORITY + 1UL),
+			(TaskHandle_t *) NULL);
+
+	vStartSimpleMQTTDemo();
+	/* Start the scheduler */
+	vTaskStartScheduler();
+
+	/* Should never arrive here */
+	return 1;
+}
